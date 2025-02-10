981353b6859d06dbaac7b030cd3ef9119dd7c568
aTadaharuKato <kato@aplix.co.jp>
2025-02-10 23:30:50 +0900

    ちょっとファイルを追加

---
+++ src/main/java/model/dao/MyConnection.java
@@ -0,0 +1,39 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class MyConnection {
+	/**
+	 * データベースURL
+	 */
+	private final static String URL = "jdbc:mysql://raspi.local/test2025a?characterEncoding=UTF-8&serverTimezone=Asia/Tokyo";
+	
+	/**
+	 * 接続ユーザ
+	 */
+	private final static String USER = "kato";
+	
+	/**
+	 * パスワード
+	 */
+	private final static String PASSWORD = "katodec25";
+
+	/**
+	 * データベース接続を行い 接続情報を返す
+	 * @return 接続情報 (Connection)
+	 * @throws SQLException
+	 * @throws ClassNotFoundException 
+	 */
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+
+		// JDBCドライバの読み込み
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+
+	}
+
+
+}
