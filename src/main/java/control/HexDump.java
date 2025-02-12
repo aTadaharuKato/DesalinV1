1812c60e5192c2d899d911eed54216410505b203
aTadaharuKato <kato@aplix.co.jp>
2025-02-12 15:11:57 +0900

    デバイスアップロード用 API CREATE-ACCESS-TOKEN 作成のための調査，検討，準備

---
+++ src/main/java/control/HexDump.java
@@ -0,0 +1,175 @@
+package control;
+
+import java.io.PrintStream;
+
+public class HexDump {
+	private HexDump() {
+	}
+
+	
+	public static void dumpCode(PrintStream ps, byte[] b, int addr, int length) {
+		// java ---> (byte) 0x13, 
+		// Kotlin -> 0x13.toByte(),
+		
+		int column = 0;
+		boolean linetop = true;
+		for (int i = 0; i < length; i++) {
+			System.out.print((linetop ? "0x" : " 0x") + HexDump.toHexStringUpper(b[i]) + ".toByte(),");
+			//System.out.print((linetop ? "(byte) 0x" : " (byte) 0x") + HexDump.toHexStringUpper(b[i]) + ",");
+			linetop = false;
+			column++;
+			if (column == 8) {
+				column = 0;
+				System.out.println();
+				linetop = true;
+			}
+		}
+		if (linetop == false) {
+			System.out.println();
+		}
+	}
+	
+	public static void dumpCodeJ(PrintStream ps, byte[] b, int addr, int length) {
+		// java ---> (byte) 0x13, 
+		// Kotlin -> 0x13.toByte(),
+		
+		int column = 0;
+		boolean linetop = true;
+		for (int i = 0; i < length; i++) {
+			//System.out.print((linetop ? "0x" : " 0x") + HexDump.toHexStringUpper(b[i]) + ".toByte(),");
+			System.out.print((linetop ? "(byte) 0x" : " (byte) 0x") + HexDump.toHexStringUpper(b[i]) + ",");
+			linetop = false;
+			column++;
+			if (column == 8) {
+				column = 0;
+				System.out.println();
+				linetop = true;
+			}
+		}
+		if (linetop == false) {
+			System.out.println();
+		}
+	}
+
+	public static void dump(PrintStream ps, byte[] b, int addr, int length) {
+		char[] ascii = new char[16];
+		int ptr = addr;
+		int column = 15;
+		for (int i = 0; i < length; i++, ptr++) {
+			column = (i % 16);
+			if (column == 0) {
+				for (int j = 0; j < 16; j++) {
+					ascii[j] = ' ';
+				}
+				ps.print(toHexStringUpper(addr) + ": ");
+				addr += 16;
+			} else if (column == 8) {
+				ps.print("- ");
+			}
+			byte v = b[ptr];
+			ascii[column] = ((v >= 0x20) & (v < 0x7F)) ? (char) v : '.';
+			ps.print(toHexStringUpper(v) + " ");
+			if (column == 15) {
+				dumpAscii(ps, ascii);
+			}
+		}
+		if (column != 15) {
+			column++;
+			int spaces = (16 - column) * 3 + ((column < 9) ? 2 : 0);
+			for (; spaces != 0; spaces--) {
+				ps.print(' ');
+			}
+			dumpAscii(ps, ascii);
+		}
+	}
+
+	private static void dumpAscii(PrintStream ps, char[] ascii) {
+		ps.print(": ");
+		for (char c: ascii) {
+			if (c == '<') {
+				ps.print("&lt;");
+			} else {
+				ps.print(c);
+			}
+		}
+		ps.println();
+	}
+
+
+	/**
+	 * バイト値を表現する 16 進文字列を取得します。
+	 * <p>
+	 * {@link #toHexString(byte)} とは a 〜 f を大文字で表現する点で異なります。
+	 * @param b バイト値を指定します。
+	 * @return b を 16 進で表現する文字列が返却されます。
+	 */
+	private static String toHexStringUpper(byte b) {
+		int v = b;
+		char[] c = new char[2];
+		for (int i = 1; i >= 0; i--) {
+			c[i] = get4bUpper(v);
+			v = v >> 4;
+		}
+		return new String(c);
+	}
+
+	/**
+	 * int 値を表現する 16 進文字列を取得します。
+	 * <p>
+	 * {@link #toHexString(int)} とは a 〜 f を大文字で表現する点で異なります。
+	 * @param v int 値を指定します。
+	 * @return v を 16 進で表現する文字列が返却されます。
+	 */
+	private static String toHexStringUpper(int v) {
+		char[] c = new char[8];
+		for (int i = 7; i >= 0; i--) {
+			c[i] = get4bUpper(v);
+			v = v >> 4;
+		}
+		return new String(c);
+	}
+
+	private static char get4bUpper(int b) {
+		b &= 0x0F;
+		return (b < 10) ? ((char) ('0' + b)) : ((char) ('A' + (b - 10)));
+	}
+	
+	
+    public static String toHexStringUpperShort(short n) {
+        char[] line = new char[4];
+        for (int i = 0; i < line.length; i++) {
+            line[i] = ' ';
+        }
+        int v = n;
+        for (int j = 3; j >= 0; j--) {
+            line[j] = get4bUpper(v);
+            v = v >> 4;
+        }
+        return new String(line);
+    }
+    public static String toHexStringUpperInt(int n) {
+        char[] line = new char[8];
+        for (int i = 0; i < line.length; i++) {
+            line[i] = ' ';
+        }
+        int v = n;
+        for (int j = 7; j >= 0; j--) {
+            line[j] = get4bUpper(v);
+            v = v >> 4;
+        }
+        return new String(line);
+    }
+
+    public static String toHexStringUpperLong(long n) {
+        char[] line = new char[16];
+        for (int i = 0; i < line.length; i++) {
+            line[i] = ' ';
+        }
+        long v = n;
+        for (int j = 15; j >= 0; j--) {
+            line[j] = get4bUpper((int) v);
+            v = v >> 4;
+        }
+        return new String(line);
+    }
+}
---
+++ src/main/java/control/TestServlet.java
@@ -0,0 +1,101 @@
+package control;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/test-servlet/*")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// URL からデバイス名を取得します.
+		// e.g. /DesalinV1/test-servlet/sakura ---> デバイス名 "sakura"
+		String requestURI = request.getRequestURI();
+		int lastIndex = requestURI.lastIndexOf("/test-servlet/");
+		String deviceName = requestURI.substring(lastIndex + "/test-servlet/".length());
+		System.out.println("🍉TestServlet#doGet: deviceName:" + deviceName);
+		
+		// リクエストボディのバイト列を取得.
+		int contentLength = request.getContentLength();
+		byte[] ba = new byte[contentLength];
+		try (InputStream is = request.getInputStream()) {
+			int rdsz;
+			for (int rdpos = 0; rdpos < contentLength; rdpos += rdsz) {
+				rdsz = is.read(ba, 0, contentLength - rdpos);
+				if (rdsz == -1) {
+					// 予期しない EOF を検出.
+					throw new IOException("unexpected requestbody length!");
+				}
+			}
+		}
+			
+		// リクエストボディのバイト列を文字列に変換.
+		String instr = new String(ba, "UTF-8");
+		System.out.println("instr:" + instr);
+		
+		// リクエストボディのバイト列より，ｌJSON オブジェクトを生成.
+		JSONObject jsonobj = new JSONObject(instr);
+		System.out.println("jsonobj:" + jsonobj);
+		
+		String password = jsonobj.optString("password");
+		String refreshtoken = jsonobj.optString("refresh");
+		if (!password.isEmpty() && !refreshtoken.isEmpty()) {
+			throw new ServletException("Both \"password\" and \"refresh\" were specified.");
+		}
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>テスト</title>");
+		out.println("</head>");
+		out.println("<body>");
+		
+		out.println("<p>");
+		out.println("getRequestURL:");
+		out.println(new String(request.getRequestURL()));
+		out.println("</p>");
+		
+		out.println("<p>");
+		out.println("getRequestURI:");
+		out.println(request.getRequestURI());
+		out.println("</p>");
+		
+		out.println("<p>requestURL.lastIndexOf(\"/test-servlet/\") = " + lastIndex + "</p>");
+		out.println("<p>deviceName:\"" + deviceName + "\"</p>");
+		out.println("<p>ContentType:\"" + request.getContentType() + "\"</p>");
+		out.println("<p>ContentLength:" + request.getContentLength() + "</p>");
+		out.println("<p>password:" + password + "</p>");
+		out.println("<p>refreshtoken:" + refreshtoken + "</p>");
+	
+		
+		out.println("<p>");
+		out.println("getServletPath:");
+		out.println(request.getServletPath());
+		out.println("</p>");
+		
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
